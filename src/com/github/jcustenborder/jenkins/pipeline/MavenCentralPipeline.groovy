fc99b74b0b54eca17ecc45edff4bffde79040244
Jeremy Custenborder <jcustenborder@gmail.com>
2017-01-24 21:53:43 -0600

    Initial pipeline for publishing to central.

---
+++ src/com/github/jcustenborder/jenkins/pipeline/MavenCentralPipeline.groovy
@@ -0,0 +1,37 @@
+package com.github.jcustenborder.jenkins.pipeline
+
+properties([
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10'))
+])
+
+
+def execute() {
+    def version
+    def artifactId
+    def description
+    def url
+
+
+    node {
+        deleteDir()
+        checkout scm
+
+        stage('build') {
+            docker.image(images.jdk8_docker_image).inside {
+                configFileProvider([configFile(fileId: 'mavenSettings', variable: 'MAVEN_SETTINGS')]) {
+                    def mvn = new MavenUtilities(env, steps, "$MAVEN_SETTINGS")
+                    version = mvn.changeVersion()
+                    artifactId = mvn.artifactId()
+                    description = mvn.description();
+                    url = mvn.url()
+                    try {
+                        mvn.execute('clean package')
+                    } finally {
+                        junit '**/target/surefire-reports/TEST-*.xml'
+                    }
+                }
+            }
+            stash includes: "target/${artifactId}-${version}.tar.gz", name: 'tar'
+        }
+    }
+}
