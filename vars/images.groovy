9b09b01f601bab2e75e49e06a80a0acc1c06eb87
Jeremy Custenborder <jcustenborder@gmail.com>
2017-09-23 14:57:25 -0500

    Changed to pass in the physical directory to the JDK.

--- vars/images.groovy
+++ vars/images.groovy
@@ -1,5 +1,9 @@
 
 def getJdk8_docker_image() {
 //    return 'maven:3.3.3-jdk-8';
-    return 'jcustenborder/packaging-centos-7:44'
+    return 'jcustenborder/packaging-centos-7:45'
+}
+
+def jdk8_java_home() {
+    return '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.144-0.b01.el7_4.x86_64'
 }
