2d61fe4502bc94dff684085d707faec74a89b3ee
Jeremy Custenborder <jcustenborder@gmail.com>
2017-09-23 15:00:36 -0500

    Corrected property name.

--- vars/images.groovy
+++ vars/images.groovy
@@ -4,6 +4,6 @@ def getJdk8_docker_image() {
     return 'jcustenborder/packaging-centos-7:45'
 }
 
-def jdk8_java_home() {
+def getJdk8_java_home() {
     return '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.144-0.b01.el7_4.x86_64'
 }
