0d0f2a1c297e165b231acbc443c90fe7d6ff3c5b
Jeremy Custenborder <jcustenborder@gmail.com>
2017-09-24 12:38:05 -0500

    Corrected `JAVA_HOME`

--- src/com/github/jcustenborder/jenkins/pipeline/MavenCentralPipeline.groovy
+++ src/com/github/jcustenborder/jenkins/pipeline/MavenCentralPipeline.groovy
@@ -20,29 +20,28 @@ def execute() {
             docker.image(images.jdk8_docker_image).inside {
                 withCredentials([file(credentialsId: 'gpg_pubring', variable: 'GPG_PUBRING'), file(credentialsId: 'gpg_secring', variable: 'GPG_SECRING')]) {
                     configFileProvider([configFile(fileId: 'mavenSettings', variable: 'MAVEN_SETTINGS')]) {
-                        def mvn = new MavenUtilities(env, steps, MAVEN_SETTINGS, GPG_PUBRING, GPG_SECRING)
-                        version = mvn.changeVersion()
-                        artifactId = mvn.artifactId()
-                        description = mvn.description();
+                        withEnv(["JAVA_HOME=${images.jdk8_java_home}"]) {
+                            def mvn = new MavenUtilities(env, steps, MAVEN_SETTINGS, GPG_PUBRING, GPG_SECRING)
+                            version = mvn.changeVersion()
+                            artifactId = mvn.artifactId()
+                            description = mvn.description();
 
-                        def goals
-                        def profiles = null
+                            def goals
+                            def profiles = null
 
-                        if (env.BRANCH_NAME == 'master') {
-                            goals = 'clean deploy'
-                            profiles = 'gpg-signing,maven-central'
-                        } else {
-                            goals = 'clean package'
-                        }
-
-
-                        url = mvn.url()
-                        try {
-                            withEnv(["JAVA_HOME=${images.jdk8_java_home}"]) {
-                                mvn.execute(goals, profiles)
+                            if (env.BRANCH_NAME == 'master') {
+                                goals = 'clean deploy'
+                                profiles = 'gpg-signing,maven-central'
+                            } else {
+                                goals = 'clean package'
                             }
-                        } finally {
-                            junit allowEmptyResults: true, testResults: '**/target/surefire-reports/TEST-*.xml'
+
+                            url = mvn.url()
+                            try {
+                                mvn.execute(goals, profiles)
+                            } finally {
+                                junit allowEmptyResults: true, testResults: '**/target/surefire-reports/TEST-*.xml'
+                            }
                         }
                     }
                 }
