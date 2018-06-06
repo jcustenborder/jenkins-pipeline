d6f7b6e4bc45e6cb31d491a76dd0356c4c103365
Jeremy Custenborder <jcustenborder@gmail.com>
2018-06-06 11:23:11 -0500

    Disable uploading connectors to maven central.

--- src/com/github/jcustenborder/jenkins/pipeline/KafkaConnectPipeline.groovy
+++ src/com/github/jcustenborder/jenkins/pipeline/KafkaConnectPipeline.groovy
@@ -175,21 +175,21 @@ def execute() {
                     }
                 }
 
-                docker.image(images.jdk8_docker_image).inside {
-                    withCredentials([file(credentialsId: 'gpg_pubring', variable: 'GPG_PUBRING'), file(credentialsId: 'gpg_secring', variable: 'GPG_SECRING')]) {
-                        configFileProvider([configFile(fileId: 'mavenSettings', variable: 'MAVEN_SETTINGS')]) {
-                            withEnv(["JAVA_HOME=${images.jdk8_java_home}", 'DOCKER_HOST=tcp://127.0.0.1:2375']) {
-                                if (env.BRANCH_NAME == 'master') {
-                                    goals = 'deploy'
-                                    profiles = 'gpg-signing,maven-central'
-
-                                    def mvn = new MavenUtilities(env, steps, MAVEN_SETTINGS, GPG_PUBRING, GPG_SECRING)
-                                    mvn.execute(goals, profiles)
-                                }
-                            }
-                        }
-                    }
-                }
+//                docker.image(images.jdk8_docker_image).inside {
+//                    withCredentials([file(credentialsId: 'gpg_pubring', variable: 'GPG_PUBRING'), file(credentialsId: 'gpg_secring', variable: 'GPG_SECRING')]) {
+//                        configFileProvider([configFile(fileId: 'mavenSettings', variable: 'MAVEN_SETTINGS')]) {
+//                            withEnv(["JAVA_HOME=${images.jdk8_java_home}", 'DOCKER_HOST=tcp://127.0.0.1:2375']) {
+//                                if (env.BRANCH_NAME == 'master') {
+//                                    goals = 'deploy'
+//                                    profiles = 'gpg-signing,maven-central'
+//
+//                                    def mvn = new MavenUtilities(env, steps, MAVEN_SETTINGS, GPG_PUBRING, GPG_SECRING)
+//                                    mvn.execute(goals, profiles)
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
             }
         }
     }
