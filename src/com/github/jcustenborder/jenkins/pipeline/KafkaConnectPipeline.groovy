f8277100addd7925ec5cc0c65fb9fa9fa360c2a2
Jeremy Custenborder <jcustenborder@gmail.com>
2018-10-20 12:32:45 -0500

    Remove support for rpm and debian packages.

--- src/com/github/jcustenborder/jenkins/pipeline/KafkaConnectPipeline.groovy
+++ src/com/github/jcustenborder/jenkins/pipeline/KafkaConnectPipeline.groovy
@@ -8,31 +8,31 @@ triggers {
     upstream(upstreamProjects: "jcustenborder/connect-utils/job/master", threshold: hudson.model.Result.SUCCESS)
 }
 
-def createPackage(String name, String type, String version, String description, String url) {
-    def inputPath = "${pwd()}/target/${name}-${version}.tar.gz"
-    def outputPath = "${pwd()}/target/${name}-${version}.${type}"
-
-    if (fileExists(outputPath)) {
-        sh "rm '${outputPath}'"
-    }
-
-    sh "/usr/local/bin/fpm --input-type tar " +
-            "--output-type ${type} " +
-            "--version ${version} " +
-            (env.BRANCH_NAME == 'master' ? "" : "--iteration ${env.BUILD_NUMBER} ") +
-            "--name ${name} " +
-            "--url ${url} " +
-            "--description '${description}' " +
-            "--license 'The Apache License, Version 2.0' " +
-            "--vendor 'Jeremy Custenborder' " +
-            "--architecture all " +
-            "--maintainer jcustenborder@gmail.com " +
-            "--config-files /etc " +
-            "--package '${outputPath}' " +
-            "'${inputPath}'"
-    echo "Finished building ${outputPath}, stashing ${outputPath}"
-    stash includes: "target/*.${type}", name: type
-}
+//def createPackage(String name, String type, String version, String description, String url) {
+//    def inputPath = "${pwd()}/target/${name}-${version}.tar.gz"
+//    def outputPath = "${pwd()}/target/${name}-${version}.${type}"
+//
+//    if (fileExists(outputPath)) {
+//        sh "rm '${outputPath}'"
+//    }
+//
+//    sh "/usr/local/bin/fpm --input-type tar " +
+//            "--output-type ${type} " +
+//            "--version ${version} " +
+//            (env.BRANCH_NAME == 'master' ? "" : "--iteration ${env.BUILD_NUMBER} ") +
+//            "--name ${name} " +
+//            "--url ${url} " +
+//            "--description '${description}' " +
+//            "--license 'The Apache License, Version 2.0' " +
+//            "--vendor 'Jeremy Custenborder' " +
+//            "--architecture all " +
+//            "--maintainer jcustenborder@gmail.com " +
+//            "--config-files /etc " +
+//            "--package '${outputPath}' " +
+//            "'${inputPath}'"
+//    echo "Finished building ${outputPath}, stashing ${outputPath}"
+//    stash includes: "target/*.${type}", name: type
+//}
 
 def execute() {
     def version
@@ -89,48 +89,47 @@ def execute() {
                     }
                 }
                 sh "ls -1 target/"
-                echo "Stashing target/*.tar.gz"
-                stash includes: "target/*.tar.gz", name: 'tar'
+//                echo "Stashing target/*.tar.gz"
+//                stash includes: "target/*.tar.gz", name: 'tar'
                 echo 'Stashing target/docs/**/**'
                 stash includes: 'target/docs/**/**', name: 'docs'
-                echo 'Stashing target/confluent-docs/**/**'
-                stash includes: 'target/confluent-docs/**/**', name: 'confluent-docs', allowEmpty: true
+//                echo 'Stashing target/confluent-docs/**/**'
+//                stash includes: 'target/confluent-docs/**/**', name: 'confluent-docs', allowEmpty: true
                 echo 'Stashing target/**/*.zip'
                 stash includes: 'target/**/*.zip', name: 'plugin', allowEmpty: true
             }
         }
     }
 
-
-    stage('os packages') {
-        parallel 'rpm': {
-            node {
-                unstash 'tar'
-                docker.image(images.jdk8_docker_image).inside {
-                    createPackage(artifactId, 'rpm', version, description, url)
-                }
-            }
-        }, 'deb': {
-            node {
-                unstash 'tar'
-                docker.image(images.jdk8_docker_image).inside {
-                    createPackage(artifactId, 'deb', version, description, url)
-                }
-            }
-        }
-    }
+//    stage('os packages') {
+//        parallel 'rpm': {
+//            node {
+//                unstash 'tar'
+//                docker.image(images.jdk8_docker_image).inside {
+//                    createPackage(artifactId, 'rpm', version, description, url)
+//                }
+//            }
+//        }, 'deb': {
+//            node {
+//                unstash 'tar'
+//                docker.image(images.jdk8_docker_image).inside {
+//                    createPackage(artifactId, 'deb', version, description, url)
+//                }
+//            }
+//        }
+//    }
 
 
     node {
-        unstash 'rpm'
-        unstash 'deb'
-        unstash 'tar'
+//        unstash 'rpm'
+//        unstash 'deb'
+//        unstash 'tar'
         unstash 'docs'
         unstash 'plugin'
 
-        archiveArtifacts artifacts: "target/*.tar.gz"
-        archiveArtifacts artifacts: "target/*.rpm", allowEmptyArchive: true
-        archiveArtifacts artifacts: "target/*.deb", allowEmptyArchive: true
+//        archiveArtifacts artifacts: "target/*.tar.gz"
+//        archiveArtifacts artifacts: "target/*.rpm", allowEmptyArchive: true
+//        archiveArtifacts artifacts: "target/*.deb", allowEmptyArchive: true
         archiveArtifacts artifacts: "target/docs/**/*"
         archiveArtifacts artifacts: "target/**/*.zip", allowEmptyArchive: true
 
