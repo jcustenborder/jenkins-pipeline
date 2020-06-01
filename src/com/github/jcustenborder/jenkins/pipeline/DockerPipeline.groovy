55f245ced12a934265e72ec946d94bbf54a31b8f
Jeremy Custenborder <jcustenborder@gmail.com>
2020-06-01 10:47:33 -0500

    Initial Commit.

---
+++ src/com/github/jcustenborder/jenkins/pipeline/DockerPipeline.groovy
@@ -0,0 +1,29 @@
+package com.github.jcustenborder.jenkins.pipeline
+
+def execute() {
+    def scmResult
+
+    node() {
+        stage('checkout') {
+            deleteDir()
+            scmResult = checkout(scm)
+        }
+
+        imageSettings = readYaml(file: 'image.yml')
+
+        def tags=[]
+
+        if (env.BRANCH_NAME == 'master') {
+            tags.add("${imageSettings['name']}:latest")
+        } else {
+            tags.add("${imageSettings['name']}:${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
+        }
+
+        def tagArgument=''
+        tags.each { tag->
+            tagArgument+=" -t '${tag}'"
+        }
+
+        sh "docker build . ${tagArgument}"
+    }
+}
