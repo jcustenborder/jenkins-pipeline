7f2995c5bf8008d7110b485f13cbcff11154897d
Jeremy Custenborder <jcustenborder@gmail.com>
2021-03-23 12:43:38 -0500

    Updated to pull from docker registry.

--- src/com/github/jcustenborder/jenkins/pipeline/MavenGitHubReleasePipeline.groovy
+++ src/com/github/jcustenborder/jenkins/pipeline/MavenGitHubReleasePipeline.groovy
@@ -10,42 +10,43 @@ def execute() {
         deleteDir()
         def scmResult = checkout(scm)
         def repositoryName = scmResult.GIT_URL.replaceAll('^.+:(.+)\\.git$', '$1')
-        docker.image(images.jdk8_docker_image).inside("--net host -e DOCKER_HOST='tcp://127.0.0.1:2375'") {
-            withCredentials([file(credentialsId: 'gpg_pubring', variable: 'GPG_PUBRING'), file(credentialsId: 'gpg_secring', variable: 'GPG_SECRING')]) {
-                configFileProvider([configFile(fileId: 'mavenSettings', variable: 'MAVEN_SETTINGS')]) {
-                    withEnv(["JAVA_HOME=${images.jdk8_java_home}", 'DOCKER_HOST=tcp://127.0.0.1:2375']) {
-                        def mvn = new MavenUtilities(env, steps, MAVEN_SETTINGS, GPG_PUBRING, GPG_SECRING)
-                        stage('build') {
-                            version = mvn.changeVersion()
-                            artifactId = mvn.artifactId()
-                            description = mvn.description();
-                            try {
-                                mvn.execute('clean test')
+        withDockerRegistry(credentialsId: 'custenborder_docker', url: 'https://docker.custenborder.com') {
+            docker.image(images.jdk11_docker_image).inside("--net host -e DOCKER_HOST='tcp://127.0.0.1:2375'") {
+                withCredentials([file(credentialsId: 'gpg_pubring', variable: 'GPG_PUBRING'), file(credentialsId: 'gpg_secring', variable: 'GPG_SECRING')]) {
+                    configFileProvider([configFile(fileId: 'mavenSettings', variable: 'MAVEN_SETTINGS')]) {
+                        withEnv(["JAVA_HOME=${images.jdk8_java_home}", 'DOCKER_HOST=tcp://127.0.0.1:2375']) {
+                            def mvn = new MavenUtilities(env, steps, MAVEN_SETTINGS, GPG_PUBRING, GPG_SECRING)
+                            stage('build') {
+                                version = mvn.changeVersion()
+                                artifactId = mvn.artifactId()
+                                description = mvn.description();
+                                try {
+                                    mvn.execute('clean test')
+                                }
+                                finally {
+                                    junit allowEmptyResults: true, testResults: '**/target/surefire-reports/TEST-*.xml'
+                                }
                             }
-                            finally {
-                                junit allowEmptyResults: true, testResults: '**/target/surefire-reports/TEST-*.xml'
+                            stage('integration-test') {
+                                try {
+                                    mvn.execute('integration-test')
+                                }
+                                finally {
+                                    junit allowEmptyResults: true, testResults: '**/target/failsafe-reports/TEST-*.xml'
+                                }
                             }
-                        }
-                        stage('integration-test') {
-                            try {
-                                mvn.execute('integration-test')
+                            stage('maven package') {
+                                mvn.execute('package')
+                                sh "ls -1 target/"
+                                echo 'Stashing target/${artifactId}-${version}.*'
+                                sh "ls -1 target/${artifactId}-${version}.*"
+                                stash includes: "target/${artifactId}-${version}.*", name: 'assembly', allowEmpty: false
                             }
-                            finally {
-                                junit allowEmptyResults: true, testResults: '**/target/failsafe-reports/TEST-*.xml'
-                            }
-                        }
-                        stage('maven package') {
-                            mvn.execute('package')
-                            sh "ls -1 target/"
-                            echo 'Stashing target/${artifactId}-${version}.*'
-                            sh "ls -1 target/${artifactId}-${version}.*"
-                            stash includes: "target/${artifactId}-${version}.*", name: 'assembly', allowEmpty: false
                         }
                     }
                 }
             }
         }
-
         stage('publish') {
             unstash 'assembly'
             archiveArtifacts artifacts: "target/${artifactId}-${version}.*"
