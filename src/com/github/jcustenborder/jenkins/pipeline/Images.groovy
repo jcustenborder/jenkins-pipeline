8dd344f9fa53a8418adf2dde458d37b9391a6ae7
Jeremy Custenborder <jcustenborder@gmail.com>
2022-04-27 14:28:16 -0500

    Updated jenkins-maven-jdk8 version.

--- src/com/github/jcustenborder/jenkins/pipeline/Images.groovy
+++ src/com/github/jcustenborder/jenkins/pipeline/Images.groovy
@@ -6,7 +6,7 @@ class Images {
 
         switch (version) {
             case 8:
-                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk8:0.0.5'
+                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk8:0.0.6'
                 break;
             case 11:
                 result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk11:0.0.2'
