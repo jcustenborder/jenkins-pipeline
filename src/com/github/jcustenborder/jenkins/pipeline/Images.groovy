a878101362c64e664bc2453a27d64b560ef53a2e
Jeremy Custenborder <jcustenborder@gmail.com>
2023-01-04 14:54:40 -0600

    Changed to use the latest image version.

--- src/com/github/jcustenborder/jenkins/pipeline/Images.groovy
+++ src/com/github/jcustenborder/jenkins/pipeline/Images.groovy
@@ -6,13 +6,13 @@ class Images {
 
         switch (version) {
             case 8:
-                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk8:0.0.7'
+                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk8:latest'
                 break;
             case 11:
-                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk11:0.0.2'
+                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk11:latest'
                 break;
             default:
-                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk8:0.0.5'
+                result = 'docker.custenborder.com/jcustenborder/jenkins-maven-jdk8:latest'
                 break;
         }
 
